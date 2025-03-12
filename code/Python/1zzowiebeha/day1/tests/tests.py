86872cc740283586a2d4b320610acf1b7ed774da
Zowie Beha <113861530+1zzowiebeha@users.noreply.github.com>
2025-03-12 16:13:30 -0400

    Rough draft tests

---
+++ code/Python/1zzowiebeha/day1/tests/tests.py
@@ -0,0 +1,15 @@
+import pytest
+
+# is this proper parent access? may it error in non-relative execution location?
+from ..main import log, parse_data, caclulate_differences
+
+def test_calculation():
+    ...
+
+def test_log_error():
+    ...
+
+def test_log_unkown_severity():
+    ...
+    
+# todo
